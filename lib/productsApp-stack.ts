d95daf075653cfa1294000eda21417df274b59f2
pauloyza <andrepaulo@id.uff.br>
2024-05-05 16:03:18 -0300

    Creation of the handler with notes

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -9,5 +9,23 @@ export class ProductsAppStack extends cdk.Stack{
 
     constructor(scope: Construct, id: string, props?: cdk.StackProps){
         super(scope, id, props)
+
+        this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(this, //scope
+            "ProductsFetchFunction", {//name which the aws will work on the console
+                //Proprieties of the lambda function
+                functionName: "ProductsFetchFunction",
+                entry: "lambda/products/productsFetchFunction.ts",
+                handler: "handler", //name of the method which will be called
+                memorySize: 128,
+                timeout: cdk.Duration.seconds(5),
+                bundling:{
+                    minify: true, //the code will be shortist to increase performance
+                    sourceMap: false //the sourcemap's resources will be desactivate to increase performance too
+                }
+                //ATTENTION WARNING: IF THE NODE.JS WAS 20.X, 2 PROPIERTIES WILL BE MANDATORY:
+                // runtime: lambda.Runtime.NODEJS_20_X
+                // memorySize: 512
+            }    	
+        )
     }
 }
