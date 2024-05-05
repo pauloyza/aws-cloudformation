3f6b11fcf9955d0324b16e8c658de243cff9b2ac
pauloyza <andrepaulo@id.uff.br>
2024-05-05 15:44:43 -0300

    class ProductsAppStacks was created with its contructor

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -2,3 +2,10 @@ import * as lambda from "aws-cdk-lib/aws-lambda"
 import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs"
 import * as cdk from "aws-cdk-lib"
 import {Construct} from "constructs"
+
+export class ProductsAppStack extends cdk.Stack{
+
+    constructor(scope: Construct, id: string, props?: cdk.StackProps){
+        super(scope, id, props)
+    }
+}
